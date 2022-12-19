efada732b779cb3eb71192df07740e37873e72b4
kkkedik <vladkeda8@gmail.com>
2022-12-19 21:53:24 +0300

    change hw4

--- src/by/itacademy/hw4/Task7.java
+++ src/by/itacademy/hw4/Task7.java
@@ -7,16 +7,12 @@ public class Task7 {
             array[i] = (int) (Math.random() * 100);
         }
         goToString(array);
-        // был вариант вывести массив в обратном порядке, но это я так понял не то
-        // можно так же создать второй массив, записать в него в обратном порядке
-        // значения и перезаписать в основном массиве значения, но я посчитал,
-        // что то, что ниже более рационально, надеюсь подрозумевалось именно это
 
-        int blank[] = new int[10];
-        for (int i = 0, p = blank.length - 1; i < array.length; i++, p--) {
-            blank[p] = array[i];
+        for (int i = 0, p = array.length - 1; i <= array.length / 2 ; i++, p--) {
+            int t = array[p];
+            array[p] = array[i];
+            array[i] = t;
         }
-        array = blank;
         goToString(array);
     }
 
