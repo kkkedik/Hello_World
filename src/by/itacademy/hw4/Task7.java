43f02a3a2ed60d120d85dc8487c32806fdab1df7
kkkedik <vladkeda8@gmail.com>
2022-12-19 22:15:46 +0300

    change hw4

--- src/by/itacademy/hw4/Task7.java
+++ src/by/itacademy/hw4/Task7.java
@@ -2,21 +2,21 @@ package by.itacademy.hw4;
 
 public class Task7 {
     public static void main(String[] args) {
-        int array[] = new int[10];
+        int array[] = new int[11];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 100);
         }
-        goToString(array);
+        printArray(array);
 
-        for (int i = 0, p = array.length - 1; i <= array.length / 2 ; i++, p--) {
+        for (int i = 0, p = array.length - 1; i < array.length / 2 ; i++, p--) {
             int t = array[p];
             array[p] = array[i];
             array[i] = t;
         }
-        goToString(array);
+        printArray(array);
     }
 
-    public static void goToString(int[] array) {
+    public static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
