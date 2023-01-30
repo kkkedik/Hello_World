442255afb59fd45e478a7403f48ae34e6dd9af2d
kkkedik <vladkeda8@gmail.com>
2023-01-30 18:57:21 +0300

    Hw12

---
+++ src/by/itacademy/hw12/task2/Task2.java
@@ -0,0 +1,55 @@
+package by.itacademy.hw12.task2;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task2 {
+    public static void main(String[] args) {
+        {
+            try (FileWriter writer = new FileWriter("file1.txt", false)) {
+                writer.write("Белая берёза\n" +
+                        "Под моим окном\n" +
+                        "Принакрылась снегом,\n" +
+                        "Точно серебром.\n" +
+                        "\n" +
+                        "На пушистых ветках\n" +
+                        "Снежною каймой\n" +
+                        "Распустились кисти\n" +
+                        "Белой бахромой.");
+                FileReader fr = new FileReader("file1.txt");
+                Scanner scanner = new Scanner(fr);
+
+                int wordsCount = 0;
+                int punctCount = 0;
+                writer.close();
+                while (scanner.hasNextLine()) {
+                    String str = scanner.nextLine();
+                    Pattern pattern = Pattern.compile("\\p{Punct}");
+
+                    String[] arrayText = str.split("\\s");
+                    for (String s : arrayText) {
+                        int count = 0;
+                        Matcher matcher = pattern.matcher(s);
+                        while (matcher.find()) {
+                            ++count;
+                        }
+                        punctCount += count;
+                        if (count != s.length()) {
+                            ++wordsCount;
+                        }
+                    }
+                    System.out.println(str);
+                }
+                System.out.println("количество слов: " + wordsCount);
+                System.out.println("колличество знаков прип-ия: " + punctCount);
+
+                fr.close();
+            } catch (IOException ex) {
+
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
