c46f99be50ea3db2185603967396fc951a8ca66f
kkkedik <vladkeda8@gmail.com>
2023-02-08 19:04:06 +0300

    hw12

---
+++ src/by/itacademy/hw122/task4/Person.java
@@ -0,0 +1,32 @@
+package by.itacademy.hw122.task4;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private String name;
+    private String surname;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String toString() {
+        return surname + " " + name + " age: " + age;
+    }
+}
+
